8ddeb5ff100967a5506f6920155c0783daf88279
Tyfyter <tyfyter2616@gmail.com>
2025-08-05 21:38:48 -0600

    Update AltVines.cs

--- Common/Hooks/AltVines.cs
+++ Common/Hooks/AltVines.cs
@@ -17,12 +17,27 @@ namespace AltLibrary.Common.Hooks {
 			VineTypeForAnchor[vine] = vine;
 			for (int i = 0; i < anchors.Length; i++) VineTypeForAnchor[anchors[i]] = vine;
 		}
+		public override void SetStaticDefaults() {
+			AddVine(TileID.JungleVines, TileID.JungleGrass);
+			AddVine(TileID.HallowedVines, TileID.HallowedGrass);
+			AddVine(TileID.CorruptVines, TileID.CorruptGrass, TileID.CorruptJungleGrass);
+			AddVine(TileID.CrimsonVines, TileID.CrimsonGrass, TileID.CrimsonJungleGrass);
+			AddVine(TileID.VineFlowers, TileID.VineFlowers);
+			AddVine(TileID.Vines, TileID.Vines);
+			AddVine(TileID.MushroomVines, TileID.MushroomGrass);
+			AddVine(TileID.AshVines, TileID.AshGrass);
+		}
 		public override bool TileFrame(int i, int j, int type, ref bool resetFrame, ref bool noBreak) {
 			if (TileID.Sets.IsVine[type]) {
 				int typeForAnchor = VineTypeForAnchor[Main.tile[i, j - 1].TileType];
-				if (typeForAnchor != -1 && typeForAnchor != type) {
+				switch (Main.tile[i, j - 1].TileType) {
+					case TileID.Grass:
+					typeForAnchor = type == TileID.VineFlowers ? TileID.VineFlowers : TileID.Vines;
+					break;
+				}
+				if (typeForAnchor != -1 && type != typeForAnchor) {
 					Main.tile[i, j].TileType = (ushort)typeForAnchor;
-					WorldGen.TileFrame(i, j + 1);
+					WorldGen.SquareTileFrame(i, j);
 				}
 			}
 			return true;
