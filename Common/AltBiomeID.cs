e3f2764cad09f8b1e56a10851d4e6fcdd228b065
Tyfyter <tyfyter2616@gmail.com>
2025-12-09 15:04:57 -0700

    Create dummy ID classes

---
+++ Common/AltBiomeID.cs
@@ -0,0 +1,14 @@
+﻿using ReLogic.Reflection;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AltLibrary.Common {
+	public class AltBiomeID {
+		[ReinitializeDuringResizeArrays]
+		public static class Sets {
+			public static SetFactory Factory { get; } = new(AltLibrary.Biomes.Count, nameof(AltBiomeID), Search);
+		}
+		/// <inheritdoc cref="IdDictionary"/>
+		public static readonly IdDictionary Search = IdDictionary.Create<AltBiomeID, int>();
+	}
+}
---
+++ Common/AltOreID.cs
@@ -0,0 +1,14 @@
+﻿using ReLogic.Reflection;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AltLibrary.Common {
+	public class AltOreID {
+		[ReinitializeDuringResizeArrays]
+		public static class Sets {
+			public static SetFactory Factory { get; } = new(AltLibrary.Ores.Count, nameof(AltOreID), Search);
+		}
+		/// <inheritdoc cref="IdDictionary"/>
+		public static readonly IdDictionary Search = IdDictionary.Create<AltOreID, int>();
+	}
+}
