e294be52471d2c4f965af5cdec27277ada0ad2ea
Tyfyter <tyfyter2616@gmail.com>
2025-12-09 18:03:04 -0700

    Fixed really messed up tile/wall biome associations

---
+++ Common/WallSets.cs
@@ -0,0 +1,13 @@
+﻿using AltLibrary.Common.AltBiomes;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AltLibrary.Common {
+	[ReinitializeDuringResizeArrays]
+	public static class WallSets {
+		public static int[] OwnedByBiomeID { get; } = WallID.Sets.Factory.CreateNamedSet(nameof(OwnedByBiomeID))
+			.RegisterIntSet(-1);
+		public static AltBiome GetOwnerBiome(int wallType) => AltLibrary.GetAltBiome(OwnedByBiomeID[wallType]);
+	}
+}
